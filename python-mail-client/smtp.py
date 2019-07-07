a5ebca30616ca4049eef139a80cef9da4820b2c6
zhixianMatthewZhao <46915203+zhixianMatthewZhao@users.noreply.github.com>
2019-07-06 18:03:51 -0700

    source code

---
+++ python-mail-client/smtp.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.message import EmailMessage
+TO = input('who do u want to send it to: ')
+SUBJECT = input('what is the subject of the email: ')
+TEXT = input('what do u want to send: ')
+rounds = input('how many times do u want to send it: ')
+msg = EmailMessage()
+gmailusername = input('what is ur email: ')
+gmailpassword = input('what is ur password: ')
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(gmailusername,gmailpassword)
+BODY = '\r\n'.join(['To: %s' % TO,'From: %s' % gmailusername,'Subject: %s'% SUBJECT, '', TEXT])
+for i in range(int(rounds)):
+    try:
+        server.sendmail(gmailusername, [TO],BODY)
+        print('Success')
+    except:
+        print('u fucked up dawg')
+server.close()
